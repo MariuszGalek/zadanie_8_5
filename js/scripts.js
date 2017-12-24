43d115382862d12ebea63e05a2a733023786762c
Mariusz Galek <mgalek1@gmail.com>
2017-12-24 13:16:21 +0100

    Add traingle area in js file

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+// scripts.js
+
+var a = prompt('Enter triangle base'),
+	h = prompt('Enter triangle height'),
+	triangleArea = a * h/2;
+
+alert('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
